abc4fb695808ce3476651ae82d35bfc498ec5b12
alex <smirnoff.awa@gmail.com>
2019-09-16 14:54:28 +0200

    Add web app

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-
+const cors = require('cors')
 const { Item } = require('./data-layer/models');
 
-const app = express();
 
+const app = express();
+app.use(cors())
 app.get('/api/items', async (req, res) => {
   res.send({ items: await Item.findAll() });
 });
