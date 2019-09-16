9672eb9060c22df393605ec9d95525cc8f60c49c
alex <smirnoff.awa@gmail.com>
2019-09-16 15:47:22 +0200

    wep app script change

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const { Item } = require('./data-layer/models');
-
+const path = require('path');
 
 const app = express();
 app.use(cors())
 
-app.get('/api/items', async (req, res) => {
-  res.send({ items: await Item.findAll() });
-});
+const PATH_TO_WEB_APP_BUILD = 'web-app/build';
+app.use(express.static(path.join(__dirname, PATH_TO_WEB_APP_BUILD)));
+app
+  .get('/api/items', async (req, res) => {
+    res.send({ items: await Item.findAll() });
+  })
+  .get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, PATH_TO_WEB_APP_BUILD, 'index.html'));
+  });
 
 const PORT = process.env.PORT || 8007;
 
